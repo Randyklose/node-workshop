5ea71bbd268b2e8d51cd11adea401bec672563c6
Randy Klose <randyklose@gmail.com>
2017-01-16 18:56:25 +0000

    Hello world exercise completed

---
+++ hello-world.js
@@ -0,0 +1,13 @@
+/*### First program
+  * Create a file called `hello-world.js` . In it, write a simple node program that outputs "`Hello World!`" 
+  to the console.
+  * Add an instruction to your program that will output "`Hello World Again!!`" 10 seconds after
+  the program was run.
+  * Save, commit and push.*/
+  
+  function sayHello() {
+      console.log("Hello World!")
+  }
+  
+  console.log("Hello World");
+  setTimeout(sayHello, 10000);
