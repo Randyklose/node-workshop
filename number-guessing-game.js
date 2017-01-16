f99dda6b9af08330498608ab8a0f8de913f5f7c6
Randy Klose <randyklose@gmail.com>
2017-01-16 22:34:10 +0000

    Guessing numbe exercise completed

---
+++ number-guessing-game.js
@@ -0,0 +1,40 @@
+/*### Number guessing game!
+  * Create a file called `number-guessing-game.js`.
+  * In this file, re-write your number guessing game (from the basic javascript workshop) for the command line!
+  * Instead of using `prompt` and `alert`, you will have to use capabilities from NodeJS and any external module.
+  **HINT**: there is an npm library called `prompt` that can help you with that :)
+  * Save/commit/push
+  */
+  var prompt = require('prompt');
+  var randomNum = Math.floor(Math.random() * 100);
+  
+  function guessNum() {
+      console.log(randomNum);
+      console.log("Guess a number between 1 and 100");
+      prompt.get(['number'],function(err, result) {
+      if(err) {
+          console.log("Error!");
+      }
+      else {
+      var number = parseInt(result.number);
+          if (number < randomNum ){
+              console.log("Try a higher number");
+              return guessNum();
+          }
+          else if(number > randomNum) {
+              console.log("Try a lower number");
+              return guessNum();
+          }
+          else if(number === randomNum) {
+              console.log("You guessed right!!!");
+          }
+          else {
+              console.log("That is not a number");
+              return guessNum();
+          }
+      }
+      
+  });
+}
+
+guessNum();
